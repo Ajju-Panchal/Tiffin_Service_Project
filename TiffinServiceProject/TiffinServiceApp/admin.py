9632e3af53d57691079d3326bbcff238c399d256
ajay-luhar <Aluhar7306@conestogac.on.ca>
2023-10-28 19:50:46 -0400

    Include all the models (userprofile, vendorprofile, address and vendortiffinservice) into admin file

--- TiffinServiceProject/TiffinServiceApp/admin.py
+++ TiffinServiceProject/TiffinServiceApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import VendorTiffinService
 
 # Register your models here.
+class VendorTiffinServiceAdmin(admin.ModelAdmin):
+    list_display = ('tiffin_service_name', 'tiffin_service_vendor', 'tiffin_service_address')
+
+admin.site.register(VendorTiffinService, VendorTiffinServiceAdmin)
--- TiffinServiceProject/vendorApp/admin.py
+++ TiffinServiceProject/vendorApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import VendorProfile
 # Register your models here.
+
+class VendorProfileAdmin(admin.ModelAdmin):
+    list_display = ('vendor', 'vendor_mobile_number')
+
+admin.site.register(VendorProfile, VendorProfileAdmin)
+
