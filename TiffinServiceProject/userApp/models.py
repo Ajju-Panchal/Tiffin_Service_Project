8d09f84b4d4875bbdf10afbb0fa20b4c872f02d0
ajay-luhar <Aluhar7306@conestogac.on.ca>
2023-10-28 19:32:10 -0400

    Created models: user profile model, address model, vendor profile model and tiffin service model

--- TiffinServiceProject/userApp/models.py
+++ TiffinServiceProject/userApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+    
+class user_or_service_address(models.Model):
+
+    # address_type = models.CharField(
+    #     max_length=20,
+    #     choices=[('vendor_address', 'Vendor Address'), ('user_address', 'User Address')],
+    # )
+    country = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    address = models.TextField()
+
+    def __str__(self):
+        return f"{self.address_type} - {self.country}, {self.city}"
+    
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_address = models.ForeignKey(user_or_service_address, on_delete=models.CASCADE)
+    mobile_number = models.CharField(max_length=20)
+    gender = models.CharField(max_length=10, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
--- TiffinServiceProject/vendorApp/models.py
+++ TiffinServiceProject/vendorApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class VendorProfile(models.Model):
+
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE)
+    vendor_mobile_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.vendor_first_name} {self.vendor_last_name}"
