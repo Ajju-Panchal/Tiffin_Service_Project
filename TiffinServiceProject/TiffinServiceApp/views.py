42f0ea3b71f13d6dfa4ca3b5b998fb18d811cce8
ajay-luhar <Aluhar7306@conestogac.on.ca>
2023-11-08 19:55:45 -0500

    Added initial view function and url to show the home page of the site

--- TiffinServiceProject/TiffinServiceApp/views.py
+++ TiffinServiceProject/TiffinServiceApp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
